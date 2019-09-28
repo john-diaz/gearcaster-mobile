617118487b7ca8289119c032403de70546b884d1
John S <diaz.john312@gmail.com>
2019-09-27 20:57:47 -0400

    minor tweaks

--- android/app/src/main/java/host/exp/exponent/MainApplication.java
+++ android/app/src/main/java/host/exp/exponent/MainApplication.java
@@ -13,6 +13,7 @@ import okhttp3.OkHttpClient;
 
 // Needed for `react-native link`
 // import com.facebook.react.ReactApplication;
+import com.instabug.reactlibrary.RNInstabugReactnativePackage;
 
 public class MainApplication extends ExpoApplication implements AppLoaderPackagesProviderInterface<ReactPackage> {
 
